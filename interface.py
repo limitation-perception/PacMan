7f042ce2beb48a716ebfca3142672b4af23c824c
La musica <sophibanit@gmail.com>
2025-12-15 22:01:52 +0200

    made a flicker, changed the packman images, started (def check_position), changed the screen size

--- interface.py
+++ interface.py
@@ -1,12 +1,14 @@
 import pygame
+from pygame import FULLSCREEN
+
 from boards_for_levels import boards
 import math
 pygame.init()
 
-WIDTH = 700
-HEIGHT = 750
+WIDTH = 900
+HEIGHT = 950
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT) ) # FULLSCREEN???
 fps = 60
 timer = pygame.time.Clock()
 # font = pygame.font.Font("", 32)      HAVE TO ADD LATER!!!!!!!!!
@@ -16,8 +18,14 @@ color_for_food = 'white'
 PI = math.pi
 pacman_images = []
 for i in range(1,5):
-    pacman_images.append(pygame.transform.scale(pygame.image.load(f'pacman_images/{i}.png'), (115, 115)))
+    pacman_images.append(pygame.transform.scale(pygame.image.load(f'packman_images/{i}.png'), (45, 45)))
 
+pacman_start_x = 450
+pacman_start_y = 663
+direction = 0
+counter = 0
+flicker = False
+turns_allowed = [False, False, False, False]
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -26,10 +34,10 @@ def draw_board():
         for j in range(len(level1[i])):
             if level1[i][j] == 1:
                 pygame.draw.circle(screen, color_for_food, (j * num2 + (0.5*num2), i * num1 +(0.5*num1)),
-                                   3)
-            if level1[i][j] == 2:
+                                   4)
+            if level1[i][j] == 2 and not flicker:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 +(0.5*num1)),
-                                   8)
+                                   10)
             if level1[i][j] == 3:
                 pygame.draw.line(screen, color_for_walls, (j * num2 + (0.5 * num2), i * num1),
                                  (j * num2 + 0.5 * num2, i * num1 + num1), 3)
@@ -52,12 +60,6 @@ def draw_board():
                 pygame.draw.line(screen, 'white',(j * num2, i * num1 +(0.5*num1)),
                                  (j * num2 + num2, i * num1 +(0.5*num1)), 3)
 
-
-
-pacman_start_x = 299
-pacman_start_y = 455
-direction = 0
-counter = 0
 def draw_pacman():
     # 0 = RIGHT     1 = LEFT    2 = UP      3 = DOWN
     if direction == 0:
@@ -69,29 +71,99 @@ def draw_pacman():
     elif direction == 3:
         screen.blit(pygame.transform.rotate(pacman_images[counter // 5], -90), (pacman_start_x, pacman_start_y))
 
+def check_position(centerx, centery):
+    turns = [False, False, False, False]
+    num1 = (HEIGHT - 50) // 32
+    num2 = WIDTH // 30
+    num3 = 15
+
+    if centerx // 30 < 29:
+        if direction == 0:
+            if level1[centery // num1][(centerx - num3) // num2] < 3:
+                turns[1] = True
+        if direction == 1:
+            if level1[centery // num1][(centerx + num3) // num2] < 3:
+                turns[0] = True
+        if direction == 2:
+            if level1[(centery + num3) // num1][centerx // num2] < 3:
+                turns[3] = True
+        if direction == 3:
+            if level1[(centery - num3) // num1][centerx // num2] < 3:
+                turns[2] = True
+
+
+        if direction == 2 or direction == 3:
+            if 12 <= centerx % num2 <= 18:
+                if level1[(centery + num3) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level1[(centery - num3) // num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centery % num1 <= 18:
+                if level1[centery // num1][(centerx - num2) // num2] < 3:
+                    turns[1] = True
+                if level1[centery // num1][(centerx + num2) // num2] < 3:
+                    turns[0] = True
+
+
+        if direction == 0 or direction == 1:
+            if 12 <= centerx % num2 <= 18:
+                if level1[(centery + num1) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level1[(centery - num1) // num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centery % num1 <= 18:
+                if level1[centery // num1][(centerx - num3) // num2] < 3:
+                    turns[1] = True
+                if level1[centery // num1][(centerx + num3) // num2] < 3:
+                    turns[0] = True
+
+
+
+    else:
+        turns[0] = True
+        turns[1] = True
+
+    return turns
+
 run = True
 while run:
     timer.tick(fps)
     if counter < 19:
         counter +=1
+        if counter > 3:
+         flicker = False
     else:
         counter = 0
+        flicker = True
 
     screen.fill('black')
     draw_board()
     draw_pacman()
+    center_x = pacman_start_x + 23
+    center_y = pacman_start_y + 24
+    turns_allowed = check_position(center_x, center_y)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                direction = 0
+                direction_command = 0
             if event.key == pygame.K_LEFT:
-                direction = 1
+                direction_command = 1
             if event.key == pygame.K_UP:
-                direction = 2
+                direction_command = 2
             if event.key == pygame.K_DOWN:
-                direction = 3
+                direction_command = 3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                direction_command = 0
+            if event.key == pygame.K_LEFT:
+                direction_command = 1
+            if event.key == pygame.K_UP:
+                direction_command = 2
+            if event.key == pygame.K_DOWN:
+                direction_command = 3
 
 
     pygame.display.flip()
