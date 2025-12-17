bde76f2de2f115161b2cfd2ab9ff3ca382ad5e5c
Sonya <limitationperception@gmail.com>
2025-12-17 15:22:52 +0200

    ghost pictures, made a font, def draw_misc, continued check_collisions

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import FULLSCREEN
 
+# from pygame import FULLSCREEN
+
 from boards_for_levels import boards
 import math
 pygame.init()
@@ -11,7 +13,7 @@ HEIGHT = 950
 screen = pygame.display.set_mode((WIDTH, HEIGHT), FULLSCREEN ) # FULLSCREEN???
 fps = 60
 timer = pygame.time.Clock()
-# font = pygame.font.Font("", 32)      HAVE TO ADD LATER
+font = pygame.font.Font("assets/fonts/PressStart2P-Regular.ttf", 32)
 level1 = boards
 color_for_walls = 'blue'
 color_for_food = 'white'
@@ -19,7 +21,15 @@ PI = math.pi
 
 pacman_images = []
 for i in range(1,5):
-    pacman_images.append(pygame.transform.scale(pygame.image.load(f'packman_images/{i}.png'), (45, 45)))
+    pacman_images.append(pygame.transform.scale(pygame.image.load(f'assets/pacman_images/{i}.png'), (45, 45)))
+
+blinky_image = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/red.png'), (45, 45))
+pinky_image = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/pink.png'), (45, 45))
+inky_image = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/blue.png'), (45, 45))
+clyde_image = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/orange.png'), (45, 45))
+dead_image = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/dead.png'), (45, 45))
+spooked_image = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/powerup.png'), (45, 45))
+
 
 pacman_x = 450 # IT`S A TOP LEFT CORNER
 pacman_y = 663
@@ -29,6 +39,37 @@ counter = 0
 flicker = False
 turns_allowed = [False, False, False, False]
 pacman_speed =  2
+score = 0
+powerup = False
+power_counter = 0
+eaten_ghost = [False, False, False, False]
+startup_counter = 0
+lives = 3
+
+
+def draw_misc():
+    score_text = font.render(f"Score: {score}", True, "white")
+    screen.blit(score_text, (10, 920))
+    if powerup:
+        pygame.draw.circle(screen, 'blue', (140, 930), 15)
+    for i in range(lives):
+        screen.blit(pygame.transform.scale(pacman_images[0], (30, 30)), (650 + i * 40, 915))
+
+def check_collisions(scor, power, power_count, eaten_ghosts):
+    num1 = (HEIGHT - 50) // 32
+    num2 = WIDTH // 30
+    if 0 < pacman_x < 870:
+        if level1[center_y // num1][center_x // num2] == 1:
+            level1[center_y // num1][center_x // num2] = 0
+            scor += 10
+        if level1[center_y // num1][center_x // num2] == 2:
+            level1[center_y // num1][center_x // num2] = 0
+            scor += 50
+            power = True
+            power_count = 0
+            eaten_ghosts = [False, False, False, False]
+
+    return scor, power, power_count, eaten_ghosts
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -151,14 +192,28 @@ while run:
     else:
         counter = 0
         flicker = True
+    if powerup and power_counter < 600:
+        power_counter +=1
+    elif powerup and power_counter >= 600:
+        power_counter = 0
+        powerup = False
+        eaten_ghost = [False, False, False, False]
+    if startup_counter < 180:
+        moving = False
+        startup_counter +=1
+    else:
+        moving = True
 
     screen.fill('black')
     draw_board()
     draw_pacman()
+    draw_misc()
     center_x = pacman_x + 23
     center_y = pacman_y + 24
     turns_allowed = check_turns(center_x, center_y)
-    pacman_x, pacman_y = move_pacman(pacman_x, pacman_y)
+    if moving:
+        pacman_x, pacman_y = move_pacman(pacman_x, pacman_y)
+    score, powerup, power_counter, eaten_ghost = check_collisions(score, powerup, power_counter, eaten_ghost)
 
 
     for event in pygame.event.get():
